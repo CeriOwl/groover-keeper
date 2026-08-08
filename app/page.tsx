543c916520036de008dc2b95f5ab4159ffebb846
Alejandro Camacho Olivares <alejandro.camacho.olivares@gmail.com>
2026-08-07 20:45:48 -0600

    feat: login mock-up done

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,30 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    <div>
+      <main className="bg-[#2C191D] w-full h-screen font-courier-prime grid place-content-center">
+        <div className="relative bg-[#FDFBF5] border-2 border-[#2C191D] w-full shadow-boxes-big shadow-[#D36951] p-10">
+          <div className="flex flex-col items-center gap-y-4">
+            <h1 className="font-bevan font-black text-4xl italic">Groove Keeper</h1>
+            <span className="uppercase text-xs font-bold text-[#BD5740] leading-1.5 tracking-widest">staff access only</span>
+          </div>
+          <div className="flex flex-col gap-y-4 pt-10">
+            <div>
+              <label className="text-xs tracking-widest font-bold uppercase text-[#5B4C4F] flex flex-col gap-y-2">
+                email
+                <input className="text-[#2C191D] text-sm bg-[#F5F1DE] border-2 border-[#2C191D] p-3 w-80" type="email" placeholder="your@groovekeeper.com" />
+              </label>
+            </div>
+            <div>
+              <label className="text-xs tracking-widest font-bold uppercase text-[#5B4C4F] flex flex-col gap-y-2">
+                password
+                <input className="text-[#2C191D] text-sm bg-[#F5F1DE] border-2 border-[#2C191D] p-3 w-80" type="password" placeholder="••••••" />
+              </label>
+            </div>
+            <a href="#" className="uppercase text-xs font-bold text-[#BD5740] leading-1.5 tracking-widest text-right underline pt-4">forgot password?</a>
+            <button className="cursor-pointer mt-6 w-fit px-6 text-md py-2 bg-[#BD5740] uppercase text-[#F5F1DE] mx-auto border-2 border-[#2C191D] shadow-boxes-small shadow-[#2C191D]">log in</button>
+          </div>
+          <span className="absolute top-0 right-10 -translate-y-1/2 px-2 py-1 -rotate-6 bg-[#E3B505] text-xs border-2 border-[#2C191D] shadow-boxes-small shadow-[#2C191D]">EST. 2026</span>
         </div>
       </main>
     </div>
