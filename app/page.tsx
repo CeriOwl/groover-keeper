53de96e43e62aaaf0acc5a2c0b0a370db4487987
Alejandro Camacho Olivares <alejandro.camacho.olivares@gmail.com>
2026-08-18 21:58:30 -0600

    feat: login jwt added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-
+"use client"
+import { signup } from "@/app/actions/auth"
+import { useActionState } from "react"
 export default function Home() {
+  const [state, action, pending] = useActionState(signup, undefined)
   return (
     <div>
       <main className="bg-[#2C191D] w-full h-screen font-courier-prime grid place-content-center">
@@ -8,25 +11,25 @@ export default function Home() {
             <h1 className="font-bevan font-black text-4xl italic">Groove Keeper</h1>
             <span className="uppercase text-xs font-bold text-[#BD5740] leading-1.5 tracking-widest">staff access only</span>
           </div>
-          <div className="flex flex-col gap-y-4 pt-10">
+          <form action={action} className="flex flex-col gap-y-4 pt-10">
             <div>
-              <label className="text-xs tracking-widest font-bold uppercase text-[#5B4C4F] flex flex-col gap-y-2">
-                email
-                <input className="text-[#2C191D] text-sm bg-[#F5F1DE] border-2 border-[#2C191D] p-3 w-80" type="email" placeholder="your@groovekeeper.com" />
+              <label htmlFor="username" className="text-xs tracking-widest font-bold uppercase text-[#5B4C4F] flex flex-col gap-y-2">
+                username
+                <input className="text-[#2C191D] text-sm bg-[#F5F1DE] border-2 border-[#2C191D] p-3 w-80" type="text" name="username" placeholder="useridk123" />
               </label>
             </div>
             <div>
-              <label className="text-xs tracking-widest font-bold uppercase text-[#5B4C4F] flex flex-col gap-y-2">
+              <label htmlFor="password" className="text-xs tracking-widest font-bold uppercase text-[#5B4C4F] flex flex-col gap-y-2">
                 password
-                <input className="text-[#2C191D] text-sm bg-[#F5F1DE] border-2 border-[#2C191D] p-3 w-80" type="password" placeholder="••••••" />
+                <input className="text-[#2C191D] text-sm bg-[#F5F1DE] border-2 border-[#2C191D] p-3 w-80" type="password" name="password" id="password" placeholder="••••••" />
               </label>
             </div>
             <a href="#" className="uppercase text-xs font-bold text-[#BD5740] leading-1.5 tracking-widest text-right underline pt-4">forgot password?</a>
-            <button className="cursor-pointer mt-6 w-fit px-6 text-md py-2 bg-[#BD5740] uppercase text-[#F5F1DE] mx-auto border-2 border-[#2C191D] shadow-boxes-small shadow-[#2C191D]">log in</button>
-          </div>
+            <button type="submit" className="cursor-pointer mt-6 w-fit px-6 text-md py-2 bg-[#BD5740] uppercase text-[#F5F1DE] mx-auto border-2 border-[#2C191D] shadow-boxes-small shadow-[#2C191D]">log in</button>
+          </form>
           <span className="absolute top-0 right-10 -translate-y-1/2 px-2 py-1 -rotate-6 bg-[#E3B505] text-xs border-2 border-[#2C191D] shadow-boxes-small shadow-[#2C191D]">EST. 2026</span>
         </div>
-      </main>
-    </div>
+      </main >
+    </div >
   );
 }
